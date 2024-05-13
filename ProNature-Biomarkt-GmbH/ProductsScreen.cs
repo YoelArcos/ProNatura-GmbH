5ad739229ae82b201eb79524266fcd2a86717cca
YoelArcos <Yoel.arcoslopez@hotmail.com>
2024-05-13 21:29:48 +0200

    Added a Database to the Project Made a connection for the Database

--- ProNature-Biomarkt-GmbH/ProductsScreen.cs
+++ ProNature-Biomarkt-GmbH/ProductsScreen.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.Remoting.Contexts;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,9 +14,12 @@ namespace ProNature_Biomarkt_GmbH
 {
     public partial class ProductsScreen : Form
     {
+        private SqlConnection databaseConnection = new SqlConnection(@"Data Source = (LocalDB)\MSSQLLocalDB; AttachDbFilename=C:\Users\Yoela\OneDrive\Documents\Pro-Natur Biomarkt GmbH.mdf;Integrated Security = True; Connect Timeout = 30");
         public ProductsScreen()
         {
             InitializeComponent();
+
+            //Start database
         }
 
         private void btnProductSafe_Click(object sender, EventArgs e)
